f1142d816c60c3cfe64a205b25ba2b1a057ff8e9
Marwenv1 <marwen.tlili@esprit.tn>
2022-01-09 16:04:56 +0100

    final
    worked

---
+++ contacts/src/App.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import Web3 from 'web3';
+import React, { Component }  from 'react';
+
+
+function App() {
+  const [account, setAccount] = useState(); // state variable to set account.
+
+  useEffect(() => {
+    async function load() {
+      const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+      const accounts = await web3.eth.requestAccounts();
+
+      setAccount(accounts[0]);
+    }
+
+    load();
+  }, []);
+
+  return (
+      <div>
+        Your account is: {account}
+      </div>
+  );
+}
+
+export default App;
